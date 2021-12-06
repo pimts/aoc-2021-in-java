81624b7b5121e7c7bcf68761f971747b123f4b69
pim <pimteslaa@gmail.com>
2021-12-06 23:48:37 +0100

    Made code more readable for solution 6

--- day-6/src/main/java/aoc/pimts/SchoolOfFish.java
+++ day-6/src/main/java/aoc/pimts/SchoolOfFish.java
@@ -5,13 +5,13 @@ import java.util.List;
 import java.util.Map;
 
 public class SchoolOfFish {
-    Map<Integer, Long> fishBucket;
+    Map<Integer, Long> fishBuckets;
 
     public SchoolOfFish(List<LanternFish> initialSchool) {
-        this.fishBucket = new HashMap<>();
-        fishBucket = createNewBucket();
-        initialSchool.forEach(lanternFish -> fishBucket.put(
-                lanternFish.getRemainingDays(), fishBucket.get(lanternFish.getRemainingDays()) + 1));
+        this.fishBuckets = new HashMap<>();
+        fishBuckets = createNewBucket();
+        initialSchool.forEach(lanternFish -> fishBuckets.put(
+                lanternFish.getRemainingDays(), fishBuckets.get(lanternFish.getRemainingDays()) + 1));
     }
 
     private Map<Integer, Long> createNewBucket() {
@@ -23,25 +23,23 @@ public class SchoolOfFish {
     }
 
     public void grow() {
-        Map<Integer, Long> newBucket = createNewBucket();
-        // Advance all fishes from remaining days 8 to 1 by one day.
-        for (int i = LanternFish.DAYS_TO_GROW; i >= 1; i--) {
-            Long current = fishBucket.get(i);
-            newBucket.put(i-1, current);
-        }
-
-        // Special case for the fishes with 0 days
-        // as they produce a new fish.
-        Long currentFishToGrow = fishBucket.get(0);
-        newBucket.put(6, fishBucket.get(7) + currentFishToGrow);
-        newBucket.put(8, currentFishToGrow);
-        fishBucket = newBucket;
+        Map<Integer, Long> newBuckets = createNewBucket();
+        newBuckets.put(0, fishBuckets.get(1));
+        newBuckets.put(1, fishBuckets.get(2));
+        newBuckets.put(2, fishBuckets.get(3));
+        newBuckets.put(3, fishBuckets.get(4));
+        newBuckets.put(4, fishBuckets.get(5));
+        newBuckets.put(5, fishBuckets.get(6));
+        newBuckets.put(6, fishBuckets.get(7) + fishBuckets.get(0));
+        newBuckets.put(7, fishBuckets.get(8));
+        newBuckets.put(8, fishBuckets.get(0));
+        fishBuckets = newBuckets;
     }
 
     public long getSizeOfSchool() {
         long sizeOfSchool = 0;
-        for (Integer integer : fishBucket.keySet()) {
-            sizeOfSchool += fishBucket.get(integer);
+        for (Integer integer : fishBuckets.keySet()) {
+            sizeOfSchool += fishBuckets.get(integer);
         }
         return sizeOfSchool;
     }
@@ -50,8 +48,8 @@ public class SchoolOfFish {
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("[ ");
-        for (Integer integer : fishBucket.keySet()) {
-            stringBuilder.append(fishBucket.get(integer));
+        for (Integer integer : fishBuckets.keySet()) {
+            stringBuilder.append(fishBuckets.get(integer));
             stringBuilder.append(" ");
         }
         stringBuilder.append("]");
